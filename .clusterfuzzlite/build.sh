19865f07d2a7b19dd8fde8a1a23dba08930d8397
timmybx <tebrennan@gwu.edu>
2026-02-08 16:25:14 -0700

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -22,12 +22,14 @@ for fuzzer in $(find . -name '*_fuzzer.py'); do
   # Package into a standalone executable
   pyinstaller --distpath $OUT --onefile --name $fuzzer_package $fuzzer
 
-  cat > $OUT/$fuzzer_basename <<EOF
+cat > "$OUT/$fuzzer_basename" <<EOF
 #!/bin/sh
+# LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname "\$0")
-ASAN_OPTIONS=\$ASAN_OPTIONS:detect_leaks=0 \\
+ASAN_OPTIONS=\$ASAN_OPTIONS:symbolize=1:external_symbolizer_path=\$this_dir/llvm-symbolizer:detect_leaks=0 \\
 PYTHONPATH="$SRC:\$PYTHONPATH" \\
 \$this_dir/$fuzzer_package "\$@"
 EOF
-  chmod +x $OUT/$fuzzer_basename
+chmod +x "$OUT/$fuzzer_basename"
+
 done
